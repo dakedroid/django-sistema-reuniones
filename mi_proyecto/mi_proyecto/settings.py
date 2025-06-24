a7f424fc1cd95c7b4f6eec511be9c55640136928
Kevin David Molina Gómez <dave1998._@hotmail.com>
2025-06-24 15:21:14 -0600

    MIGRACIONES TERMINADAS

--- mi_proyecto/mi_proyecto/settings.py
+++ mi_proyecto/mi_proyecto/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'mi_aplication',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'mi_proyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodb',
+        'USER': 'django',
+        'PASSWORD': 'Mapa1000!',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
